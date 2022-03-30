f100ee7f7088581b53e92e98a1d391b25acf32bc
Muzammil <muzamilbhundi@gmail.com>
2022-03-31 00:31:20 +0500

    Typewriter effect

--- components/Home/Banner.js
+++ components/Home/Banner.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import { Flex,Heading,Stack,Text,Box } from '@chakra-ui/react';
 import CommonButton from '../common/CommonButton';
-
+import Typewriter from "typewriter-effect";
+import CommonHeading from '../common/CommonHeading';
 export default function Banner() {
   return (
     <>
     <Flex color='white' bgColor='primary' justifyContent='center'>
       <Flex direction='column'>
         <Stack textAlign={'center'} spacing={4} my={20}>
-          <Heading as='h1' >Graphic <Box as='span' color='secondary' textDecoration={'underline'} > Designing</Box> Services</Heading>
+          {/* <Heading as='h1' >Graphic <Box as='span' color='secondary' textDecoration={'underline'} > Designing</Box> Services</Heading> */}
+          <CommonHeading as='h1' textDecoration='underline' color='white'>
+          <Typewriter
+  
+       onInit={(typewriter)=> {
+  
+       typewriter
+        
+       .typeString("GRAPHIC DESIGNING SERVICES")
+         
+       .pauseFor(1000)
+       .deleteAll()
+       .typeString(`VIDEO EDITING SERVICES`)
+       .pauseFor(1000)
+       .deleteAll()
+       .typeString("WEBSITE DEVELOPMENT SERVICES")
+       .pauseFor(1000)
+       .deleteAll()
+       .typeString("APPLICATION DEVELOPMENT SERVICES")
+       .start();
+       }}
+       />
+          </CommonHeading>
           <Text>
           SKILLATRIA PROVIDES YOU THE <Box as='span' color='secondary'> SOLUTION</Box> OF YOUR BUSINESSES TO GROW IT DIGITALLY <br/> WITH ITS CREATIVITY AND SERVICES
           </Text>
-          <Flex justifyContent='center' gap={20} pt={4}>
+          <Flex justifyContent='center' gap={{base:10,sm:20}} p={4}>
             <CommonButton label='GET IN TOUCH' />
             <CommonButton label='OUR SERVICES' />
           </Flex>
--- components/VideoEditingPage/VideoSections.js
+++ components/VideoEditingPage/VideoSections.js
@@ -1,19 +1,24 @@
 import { AspectRatio, Stack, Text,Box } from "@chakra-ui/react";
 import React from "react";
 import CommonHeading from '../common/CommonHeading';
-import ReactPlayer from "react-player/lazy";
 import CommonButton from '../common/CommonButton';
 
 export default function VideoSections(props) {
   return (
     <>
-      <Stack alignItems='center' my={8} >
+      <Stack alignItems='center' my={8} px={2}  >
         <CommonHeading>{props.title}</CommonHeading>
-        <Text textAlign="center" width={{sm:"75%"}}>{props.description} </Text>
-        <Box py={6}>
-          <ReactPlayer url={props.video.src} title={props.video.title} controls autoplay />
-        </Box>
-        <CommonButton label='Get Quote'  />
+        <Text textAlign="center"  width={{sm:"75%"}} pb={4}>{props.description} </Text>
+        {/* <Flex> */}
+        <AspectRatio   width='100%' maxW='560px' height={{base:'50vw',md:'315px'}} ratio={'16/9'}>
+          {/* <ReactPlayer url= title={} controls autoplay /> */}
+          {/* <Box > */}
+          <iframe  src={props.video.src} title={props.video.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          {/* </Box> */}
+        </AspectRatio>
+        {/* </Flex> */}
+        <Box pb={1} />
+        <CommonButton  label='Get Quote'  />
       </Stack>
     </>
   );

8ee17a73ed081d587b2b3c92e8cd0d78d9258f17
Muzammil <muzamilbhundi@gmail.com>
2022-03-31 00:35:33 +0500

    lint error

--- components/VideoEditingPage/VideoSections.js
+++ components/VideoEditingPage/VideoSections.js
@@ -13,7 +13,7 @@ export default function VideoSections(props) {
         <AspectRatio   width='100%' maxW='560px' height={{base:'50vw',md:'315px'}} ratio={'16/9'}>
           {/* <ReactPlayer url= title={} controls autoplay /> */}
           {/* <Box > */}
-          <iframe  src={props.video.src} title={props.video.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe  src={props.video.src} title={props.video.title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           {/* </Box> */}
         </AspectRatio>
         {/* </Flex> */}
